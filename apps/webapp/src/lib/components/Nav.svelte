66a936a1b5031ece5beebfaca8a6936265c61762
Jack Kufa <jack@kufa.io>
2026-02-15 13:25:38 -0500

    feat(webapp): add repo nav button and adjust +page padding

--- apps/webapp/src/lib/components/Nav.svelte
+++ apps/webapp/src/lib/components/Nav.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Github } from '@lucide/svelte';
+  import * as Button from '$lib/components/ui/button';
   import * as NavigationMenu from '$lib/components/ui/navigation-menu';
 </script>
 
@@ -10,4 +12,15 @@
       <!-- nav items go here -->
     </NavigationMenu.List>
   </NavigationMenu.Root>
+
+  <Button.Root
+    href="https://github.com/jkufa/super-duper-eureka"
+    target="_blank"
+    rel="noreferrer"
+    variant="outline"
+    class="ms-auto"
+  >
+    <Github class="size-4" />
+    View repo
+  </Button.Root>
 </nav>
